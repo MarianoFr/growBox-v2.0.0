9caabf277b4756cf08da5de59c7ec2c1803eb7ad
MarianoFr <marianofranceschetti@gmail.com>
2023-10-25 16:48:12 -0300

    alternate water

--- src/debug_flags.h
+++ src/debug_flags.h
@@ -2,16 +2,16 @@
 #define DEBUG_FLAGS_H
 
 #define USE_RTC 1
-#define SERIAL_DEBUG 0
+#define SERIAL_DEBUG 1
 #define RGB_DEBUG 0
 #define RTC_DEBUG 0
-#define HTU_DEBUG 0
+#define HTU_DEBUG 1
 #define TIME_DEBUG 0
 #define FIRE_DEBUG  0
 #define ESPtag_DEBUG 0
 #define BH_DEBUG 0
-#define SOIL_DEBUG 0
-#define WIFI_DEBUG 0
-#define NTP_RETRY 3 
+#define SOIL_DEBUG 1
+#define WIFI_DEBUG 1
+#define NTP_RETRY 5
 
 #endif
